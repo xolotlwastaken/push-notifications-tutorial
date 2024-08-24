b93ad55f02aae0fd5a79ff3931178cfb2196e6b6
just_xolotl <157688992+xolotlwastaken@users.noreply.github.com>
2024-08-24 21:40:54 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,11 +2,14 @@ import { createClient } from 'npm:@supabase/supabase-js@2'
 import { JWT } from 'npm:google-auth-library@9'
 import serviceAccount from '../service-account.json' with { type: 'json' }
 
+
+// this has to be the same column names as your Notifications Table
 interface Notification {
-  id: string
-  user_id: string
-  body: string
+  id: int
+  user_id: string  // this is the uid of the person receiving the notification
+  body: string  // this is the body text in the push notification
 }
+
 
 interface WebhookPayload {
   type: 'INSERT'
@@ -15,26 +18,32 @@ interface WebhookPayload {
   schema: 'public'
 }
 
+
 const supabase = createClient(
   Deno.env.get('SUPABASE_URL')!,
   Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
 )
 
+
 Deno.serve(async (req) => {
   const payload: WebhookPayload = await req.json()
 
+
   const { data } = await supabase
-    .from('profiles')
-    .select('fcm_token')
+    .from('profiles')  // change this to name of table where fcm_token is stored
+    .select('fcm_token') // change this to the name of the column where fcm_token is stored
     .eq('id', payload.record.user_id)
     .single()
 
+
   const fcmToken = data!.fcm_token as string
+
 
   const accessToken = await getAccessToken({
     clientEmail: serviceAccount.client_email,
     privateKey: serviceAccount.private_key,
   })
+
 
   const res = await fetch(
     `https://fcm.googleapis.com/v1/projects/${serviceAccount.project_id}/messages:send`,
@@ -48,7 +57,7 @@ Deno.serve(async (req) => {
         message: {
           token: fcmToken,
           notification: {
-            title: `Notification from Supabase`,
+            title: `Notification from Supabase`,  // change this to change the title of the push notification
             body: payload.record.body,
           },
         },
@@ -56,15 +65,18 @@ Deno.serve(async (req) => {
     }
   )
 
+
   const resData = await res.json()
   if (res.status < 200 || 299 < res.status) {
     throw resData
   }
 
+
   return new Response(JSON.stringify(resData), {
     headers: { 'Content-Type': 'application/json' },
   })
 })
+
 
 const getAccessToken = ({
   clientEmail,
