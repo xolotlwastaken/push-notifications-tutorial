12fe10f46bd66860ab73713d165d81b6461f02dc
just_xolotl <157688992+xolotlwastaken@users.noreply.github.com>
2024-09-10 00:09:11 +0800

    Update initializeMessaging.dart

--- initializeMessaging.dart
+++ initializeMessaging.dart
@@ -1,4 +1,5 @@
 // Automatic FlutterFlow imports
+import '/backend/schema/structs/index.dart';
 import '/backend/supabase/supabase.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
